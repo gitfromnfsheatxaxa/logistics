0bfbc507e1efbb012392213e0ee5b7e47ebd81dc
gitfromnfsheatxaxa <xadasad67@gmail.com>
2024-11-15 19:57:47 +0500

    final

--- src/components/HomeCom/HomeCom.jsx
+++ src/components/HomeCom/HomeCom.jsx
@@ -2,17 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./HomeCom.css";
 
 const HomeCom = () => {
-    const [bgLoaded, setBgLoaded] = useState(false);
 
-    useEffect(() => {
-        const handleLoad = () => setBgLoaded(true);
-        window.addEventListener("load", handleLoad);
-
-        return () => window.removeEventListener("load", handleLoad);
-    }, []);
 
     return (
-        <div className={`header-img ${bgLoaded ? "loaded" : ""}`}>
+        <div className="header-img loaded">
             <div className="header-linear"></div>
         </div>
     );
