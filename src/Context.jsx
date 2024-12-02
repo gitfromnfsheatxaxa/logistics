d3ee532e9bc97f054d76633b143b5da9a53f6bcf
Gitfromnfsheatxaxax <100586658+gitfromnfsheatxaxa@users.noreply.github.com>
2024-12-02 18:17:28 +0500

    Update Context.jsx

--- src/Context.jsx
+++ src/Context.jsx
@@ -5,8 +5,7 @@ export const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
     const [array, setArray] = useState([]);
-    // const url = 'http://63.141.255.241:8080/api/collections/trucks/records';
-    const url = "";
+    const url = 'http://63.141.255.241:8080/api/collections/trucks/records';
     const getData = async () => {
         try {
             const response = await fetch(url);
